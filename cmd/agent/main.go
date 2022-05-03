94c2a2cdfb0b56a43b6ebf752feb4c997683e139
Александр Морев <a.morev@kryptonite.ru>
2022-05-03 14:39:23 +0300

    Add scheduler

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -8,59 +8,63 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	log "github.com/sirupsen/logrus"
 
 	"github.com/hikjik/go-musthave-devops-tpl.git/internal/config"
 	"github.com/hikjik/go-musthave-devops-tpl.git/internal/metrics"
+	"github.com/hikjik/go-musthave-devops-tpl.git/internal/scheduler"
 )
+
+type agent struct {
+	collector *metrics.Collector
+	key       string
+	address   string
+}
+
+func (a *agent) sendMetrics() {
+	collection := a.collector.ListMetrics()
+	if a.key != "" {
+		for _, metric := range collection {
+			if err := metrics.Sign(metric, a.key); err != nil {
+				log.Warnf("Failed to set hash: %v", err)
+			}
+		}
+	}
+
+	data, err := json.Marshal(collection)
+	if err != nil {
+		log.Warnf("Failed to marshal metrics")
+		return
+	}
+	url := fmt.Sprintf("http://%s/updates/", a.address)
+	response, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		log.Warnf("Failed to post metric: %v", err)
+		return
+	}
+	if err := response.Body.Close(); err != nil {
+		log.Warnf("Failed to close response body: %v", err)
+	}
+}
 
 func main() {
 	cfg := config.GetAgentConfig()
 
 	collector := metrics.NewCollector()
+	a := &agent{
+		collector: collector,
+		key:       cfg.Key,
+		address:   cfg.Address,
+	}
 
-	reportTicker := time.NewTicker(cfg.ReportInterval)
-	pollTicker := time.NewTicker(cfg.PollInterval)
+	s := scheduler.New()
+	defer s.Stop()
+	s.Add(collector.UpdateRuntimeMetrics, cfg.PollInterval)
+	s.Add(collector.UpdateUtilizationMetrics, cfg.PollInterval)
+	s.Add(a.sendMetrics, cfg.ReportInterval)
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
-
-	for {
-		select {
-		case <-pollTicker.C:
-			go collector.UpdateRuntimeMetrics()
-			go collector.UpdateUtilizationMetrics()
-		case <-reportTicker.C:
-			go func() {
-				collection := collector.ListMetrics()
-				if cfg.Key != "" {
-					for _, metric := range collection {
-						if err := metrics.Sign(metric, cfg.Key); err != nil {
-							log.Warnf("Failed to set hash: %v", err)
-						}
-					}
-				}
-
-				data, err := json.Marshal(collection)
-				if err != nil {
-					log.Warnf("Failed to marshal metrics")
-					return
-				}
-				url := fmt.Sprintf("http://%s/updates/", cfg.Address)
-				response, err := http.Post(url, "application/json", bytes.NewBuffer(data))
-				if err != nil {
-					log.Warnf("Failed to post metric: %v", err)
-					return
-				}
-				if err := response.Body.Close(); err != nil {
-					log.Warnf("Failed to close response body: %v", err)
-				}
-
-			}()
-		case <-sig:
-			return
-		}
-	}
+	<-sig
 }
---
+++ internal/scheduler/scheduler.go
@@ -0,0 +1,46 @@
+package scheduler
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+type Scheduler struct {
+	wg            *sync.WaitGroup
+	cancellations []context.CancelFunc
+}
+
+func New() *Scheduler {
+	return &Scheduler{
+		wg:            new(sync.WaitGroup),
+		cancellations: make([]context.CancelFunc, 0),
+	}
+}
+
+func (s *Scheduler) Add(f func(), interval time.Duration) {
+	ctx, cancel := context.WithCancel(context.Background())
+	s.cancellations = append(s.cancellations, cancel)
+
+	s.wg.Add(1)
+	go func() {
+		defer s.wg.Done()
+
+		ticker := time.NewTicker(interval)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				f()
+			}
+		}
+	}()
+}
+
+func (s *Scheduler) Stop() {
+	for _, cancel := range s.cancellations {
+		cancel()
+	}
+	s.wg.Wait()
+}
