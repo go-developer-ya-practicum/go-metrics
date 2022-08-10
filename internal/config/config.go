655adfd693b7e3ff401b52247ba7cb69558a3949
Александр Морев <a.morev@kryptonite.ru>
2022-08-11 01:57:32 +0300

    feat: config from json file

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,9 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"os"
 	"time"
 
 	"github.com/caarlos0/env/v6"
@@ -11,38 +13,48 @@ import (
 
 // AgentConfig содержит настройки агента по сбору метрик
 type AgentConfig struct {
-	Address        string        `env:"ADDRESS"`
-	SignatureKey   string        `env:"KEY"`
-	PublicKeyPath  string        `env:"CRYPTO_KEY"`
-	PollInterval   time.Duration `env:"POLL_INTERVAL"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
+	Address        string        `env:"ADDRESS" json:"address"`
+	SignatureKey   string        `env:"KEY" json:"key"`
+	PublicKeyPath  string        `env:"CRYPTO_KEY" json:"crypto_key"`
+	PollInterval   time.Duration `env:"POLL_INTERVAL" json:"poll_interval"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL" json:"report_interval"`
 }
 
 // StorageConfig содержит настройки хранилища метрик
 type StorageConfig struct {
-	StoreFile     string        `env:"STORE_FILE"`
-	DatabaseDNS   string        `env:"DATABASE_DSN"`
-	StoreInterval time.Duration `env:"STORE_INTERVAL"`
-	Restore       bool          `env:"RESTORE"`
+	StoreFile     string        `env:"STORE_FILE" json:"store_file"`
+	DatabaseDNS   string        `env:"DATABASE_DSN" json:"database_dsn"`
+	StoreInterval time.Duration `env:"STORE_INTERVAL" json:"store_interval"`
+	Restore       bool          `env:"RESTORE" json:"restore"`
 }
 
 // ServerConfig содержит настройки сервера по сбору рантайм-метрик
 type ServerConfig struct {
-	Address           string `env:"ADDRESS"`
-	SignatureKey      string `env:"KEY"`
-	EncryptionKeyPath string `env:"CRYPTO_KEY"`
+	Address           string `env:"ADDRESS" json:"address"`
+	SignatureKey      string `env:"KEY" json:"key"`
+	EncryptionKeyPath string `env:"CRYPTO_KEY" json:"crypto_key"`
 	StorageConfig     StorageConfig
 }
 
 // GetAgentConfig возвращает настройки AgentConfig
 func GetAgentConfig() AgentConfig {
 	var config AgentConfig
+	var path string
 
 	flag.StringVar(&config.Address, "a", "127.0.0.1:8080", "Server address")
 	flag.DurationVar(&config.PollInterval, "p", time.Second*2, "Poll interval, sec")
 	flag.DurationVar(&config.ReportInterval, "r", time.Second*10, "Report interval, sec")
 	flag.StringVar(&config.SignatureKey, "k", "", "HMAC key")
 	flag.StringVar(&config.PublicKeyPath, "crypto-key", "", "Path to public RSA key")
+	flag.StringVar(&path, "c", "", "Path to json config file")
+	flag.StringVar(&path, "config", "", "Path to json config file")
+	flag.Parse()
+
+	if err := parseConfigJSON(&config, path); err != nil {
+		log.Fatal().Err(err).Msg("Failed to parse agent json config")
+	}
+
+	// second call for correct priority
 	flag.Parse()
 
 	if err := env.Parse(&config); err != nil {
@@ -55,6 +67,7 @@ func GetAgentConfig() AgentConfig {
 // GetServerConfig возвращает настройки ServerConfig
 func GetServerConfig() ServerConfig {
 	var config ServerConfig
+	var path string
 
 	flag.StringVar(&config.Address, "a", "127.0.0.1:8080", "Server Address")
 	flag.StringVar(&config.SignatureKey, "k", "", "HMAC key")
@@ -63,6 +76,15 @@ func GetServerConfig() ServerConfig {
 	flag.BoolVar(&config.StorageConfig.Restore, "r", true, "Restore After Start")
 	flag.StringVar(&config.StorageConfig.DatabaseDNS, "d", "", "Database DNS")
 	flag.StringVar(&config.EncryptionKeyPath, "crypto-key", "", "Path to private RSA key")
+	flag.StringVar(&path, "c", "", "Path to json config file")
+	flag.StringVar(&path, "config", "", "Path to json config file")
+	flag.Parse()
+
+	if err := parseConfigJSON(&config, path); err != nil {
+		log.Fatal().Err(err).Msg("Failed to parse server json config")
+	}
+
+	// second call for correct priority
 	flag.Parse()
 
 	if err := env.Parse(&config); err != nil {
@@ -70,4 +92,18 @@ func GetServerConfig() ServerConfig {
 	}
 
 	return config
+}
+
+func parseConfigJSON(cfg interface{}, path string) error {
+	if path != "" {
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		if err = json.Unmarshal(data, cfg); err != nil {
+			return err
+		}
+	}
+	return nil
 }
